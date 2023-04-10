1187d956d31ef932fe04699d5ff4c23bb1382d42
JPAlkamim <jpalkamim@gmail.com>
2023-04-10 15:06:44 -0300

    finalizei o trabalho com os plots

--- SVMRestante.py
+++ SVMRestante.py
@@ -18,7 +18,7 @@ for train_index, test_index in skf.split(X, y):
     X_train, X_test = X[train_index], X[test_index]
     y_train, y_test = y[train_index], y[test_index]
 
-    svc = SVC(kernel='rbf', gamma=1, C=1)
+    svc = SVC(kernel='rbf', gamma=1, C=1000)
 
     svc.fit(X_train, y_train)
 
